b00ee466aa293bff7023f1bf659000352d3a08c2
WAI XIONG CHEE <wai.xiong@setel.my>
2021-06-12 16:03:44 +0800

    prod

---
+++ backend/fsktm-deploy-prod.sh
@@ -0,0 +1,2 @@
+scp ./docker-compose-fsktm-prod.yaml dinowex@103.18.2.220:~/vexs/server/docker-compose.yaml
+scp ./vexs.tar dinowex@103.18.2.220:~/vexs/server/vexs.tar
---
+++ fsktm-deploy-prod.sh
@@ -0,0 +1,3 @@
+scp ./docker-compose-fsktm-prod.yaml dinowex@103.18.2.220:~/vexs/client/docker-compose.yaml
+scp ./Dockerfile dinowex@103.18.2.220:~/vexs/client/Dockerfile
+scp ./build.zip dinowex@103.18.2.220:~/vexs/client/build.zip
