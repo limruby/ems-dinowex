0b987248908d080285169845a3d31d45316d9ce3
WAI XIONG CHEE <wai.xiong@setel.my>
2021-06-20 16:40:44 +0800

    back cd

--- _build_deploy_dev.sh
+++ _build_deploy_dev.sh
@@ -11,3 +11,5 @@ sudo docker build -t vexsdev/backend .
 
 # deploy
 sudo docker-compose -f docker-compose-fsktm.yaml up -d
+
+cd ../
--- _build_deploy_prod.sh
+++ _build_deploy_prod.sh
@@ -11,3 +11,5 @@ sudo docker build -t vexs/backend .
 
 # deploy
 sudo docker-compose -f docker-compose-fsktm-prod.yaml up -d
+
+cd ../
