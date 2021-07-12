0653899a84717b69c35bb45c48902a0f06036de5
WAI XIONG CHEE <wai.xiong@setel.my>
2021-07-12 09:15:01 +0800

    prod

--- _build_deploy_prod.sh
+++ _build_deploy_prod.sh
@@ -7,7 +7,7 @@ rm -r ./build
 
 # build server
 cd ./backend
-sudo docker build -t vexs/backend . 
+sudo docker build -t vexs/backend_prod . 
 
 # deploy
 sudo docker-compose -f docker-compose-fsktm-prod.yaml up -d
