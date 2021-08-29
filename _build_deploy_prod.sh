7538841e8f60296c67a85b73cfd59de7c8517f89
WAI XIONG CHEE <wai.xiong@setel.my>
2021-08-29 13:48:53 +0800

    insecure connection

--- _build_deploy_prod.sh
+++ _build_deploy_prod.sh
@@ -9,6 +9,17 @@ rm -r ./build
 cd ./backend
 sudo docker build -t vexs/backend_prod . 
 
+# build insecure web
+rm -r ./backend/public
+npm install
+npm run build:insecure
+cp -r build ./backend/public
+rm -r ./build
+
+# build insecure server
+cd ./backend
+sudo docker build -t vexs/backend_prod_insecure .  
+
 # deploy
 sudo docker-compose -f docker-compose-fsktm-prod.yaml up -d
 
