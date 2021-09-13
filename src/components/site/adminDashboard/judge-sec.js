e920c9229c02603dbac765b25783a2c1c91d7ad7
Yuzhi <limruby97@gmail.com>
2021-09-13 22:31:02 +0800

    admin can export judges list to csv

--- src/components/site/adminDashboard/judge-sec.js
+++ src/components/site/adminDashboard/judge-sec.js
@@ -1,12 +1,29 @@
 import React, { useState, useEffect } from "react";
 import Table from './Table.js';
 import { Link } from 'react-router-dom';
+import { CSVLink } from 'react-csv';
 import axiosInstance from '../../../utils/axiosConfig';
 
 function Judge() {
   const [data, setData] = useState([]);
   const [link, setLink] = useState([])
-
+  const headers = [
+    { label: 'Title', key: 'title' },
+    { label: 'Full Name', key: 'name' },
+    { label: 'Phone Number', key: 'phone_no' },
+    { label: 'Affiliation', key: 'affiliation' },
+    { label: 'Address_1', key: 'address_1' },
+    { label: 'Address_2', key: 'address_2' },
+    { label: 'Postcode', key: 'postcode' },
+    { label: 'City', key: 'city' },
+    { label: 'State', key: 'state' },
+    { label: 'Country', key: 'country' },
+  ]
+  const csvReport = {
+    filename: 'Dinowex_Judges_List.csv',
+    headers: headers,
+    data: data
+  }
   useEffect(() => {
     axiosInstance.get("/api/judge/readAll")
       .then(function (response) {
@@ -16,7 +33,7 @@ function Judge() {
       })
     axiosInstance.get("/api/formLink/read")
       .then(function (response) {
-        setLink(response.data.data);  
+        setLink(response.data.data);
       }).catch(function (error) {
         console.log(error);
       })
@@ -90,6 +107,8 @@ function Judge() {
   return (
     <div className="App" id="competitor">
       <Table columns={columns} data={data} />
+      <CSVLink {...csvReport}>
+        <button className="btn btn-success" >Export to CSV</button></CSVLink>
     </div>
   );
 }
