9517e7ca0f772b0043ecded7a0f104ea226d47dd
alexia97a <74533213+alexia97a@users.noreply.github.com>
2021-11-13 17:53:02 +0800

    admin toggle

--- backend/controllers/CompetitorController.js
+++ backend/controllers/CompetitorController.js
@@ -156,6 +156,12 @@ const update = (req, res, next) => {
   }
   if (req.body.first_purchase) {
     updateCompetitor['first_purchase'] = req.body.first_purchase;
+  } 
+  if (req.body.bill_status) {
+    updateCompetitor['bill_status'] = req.body.bill_status;
+  }
+  if (req.body.bill_id) {
+    updateCompetitor['bill_id'] = req.body.bill_id;
   }
 
 
